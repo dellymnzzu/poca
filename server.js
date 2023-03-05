b77db8296166810be3db0bc1712e283526d63dde
dellymnzzu <fudnr4010@naver.com>
2023-03-06 06:41:06 +0900

    changes,
    전체- 줄 맞춤
    server.js - 찜페이지 생성
    bestLike - 좋아요 숫자 보여주기
    mypage - 마이페이지 생성

--- server.js
+++ server.js
@@ -24,61 +24,39 @@ app.use(passport.session());
 MongoCilent.connect('mongodb+srv://admin:dlsgud5031@cluster0.7igrfxz.mongodb.net/poca?retryWrites=true&w=majority',(에러,client)=>{
 if(에러) return console.log(에러);
 db = client.db('poca');
-
 app.post('/sign',(req,res)=>{
-
 res.send('저장완료되었습니다.')
-
-
-db.collection('login').insertOne({ id : req.body.id,pw :req.body.pw,name:req.body.name,Email:req.body.Email},(에러,결과)=>{
-console.log(req.body);
-})
-})
-
-});
-
-
+db.collection('login').insertOne({ id : req.body.id,pw :req.body.pw,name:req.body.name,Email:req.body.Email})})
+}) // 회원가입
 
 app.listen(8080);
 
 app.get('/',(req,res)=>{
-    
-    
+  
 db.collection('content').find().sort({"시간":-1}).toArray((error,result)=>{
 
     if(error) {
-        console.log(error);
+        console.log('서버'+error);
     }
-
-    console.log(result);
+    console.log('8080서버 열림');
     res.render('index.ejs',{Module:result});
-
-
-
 })
 })
+//index페이지
 
 app.get('/newmodule/:id',(req,res)=>{
-
-    console.log(req.params.id);
     db.collection('content').findOne({_id:parseInt(req.params.id)},(error,result)=>{
         if(error) return console.log('error');
-        console.log(result);
         res.render('newmodule.ejs',{Module:result});
-
     })
 })
-
-
+//포카모듈
 
 app.get('/sign_up',(req,res)=>{
-console.log('회원가입 서버 열림');
 res.render('sign_up.ejs')    //회원가입페이지
 })
 
 app.get('/login',(req,res)=>{
-
-console.log('login 서버 열림');
 res.render('login');   //로그인페이지 만들기
 })
 
@@ -86,7 +64,7 @@ app.post('/login',passport.authenticate('local', {failureRedirect : '/fail'}),(r
 console.log(req.body);
 res.redirect('/')    
 })
-
+//로그인 후 index페이지 
 passport.use(new LocalStrategy({
 usernameField: 'id',
 passwordField: 'pw',
@@ -116,21 +94,25 @@ db.collection('login').findOne({id:아이디},(error,result)=>{
 done(null, result)
 })
 }); 
-
-
+//아이디,비번 미들웨어
 app.get('/about',(req,res)=>{
-console.log('gallery서버 열림');
-res.render('about')   // 사진 페이지 만들기
-
+res.render('about') 
 });
+//about페이지
+app.get('/mypage',로그인했니,(req,res)=>{
+    db.collection('content').find({likeid:req.user.id}).sort({"시간":-1}).toArray((error,result)=>{
+        res.render('mypage.ejs',{id:req.user,Module:result,contents:result});
+    })   
+ })
+ //마이페이지
+
+app.get('/mypage/my',로그인했니,(req,res)=>{
+    db.collection('content').find({작성자:req.user.id}).toArray((error,result)=>{
+            res.render('mypage',{id:req.user,Module:result,contents:result});
+    })
+}) // 마이페이지 중 내상품 페이지
 
 
-
-app.get('/mypage',로그인했니,(req,res)=>{ 
-console.log(req.user);
-
-res.render('mypage',{id:req.user});
-})
 
 function 로그인했니(req,res,next){
 
@@ -144,6 +126,7 @@ res.render('login.ejs');
 
 }
 }
+//로그인했니 함수
 
 
 app.post('/sign',(req,res)=>{
@@ -151,14 +134,11 @@ db.collection('login').insertOne({name:req.body.name,id:req.body.id,pw:req.body.
 console.log('회원가입완료');
 res.redirect('/login');
 })
-})
-
+}) // 회원가입 
 
 app.get('/write',로그인했니,(req,res)=>{
-
-
 res.render('write.ejs');
-})
+}) //글쓰기페이지
 
 
 app.post('/add',(req,res)=>{
@@ -169,7 +149,12 @@ db.collection('counter').findOne({name:'총게시물갯수'},(error,result)=>{
 console.log(result.totalPost);
 var 총게시물갯수=result.totalPost;
 
-var 저장할거 = {_id:총게시물갯수+1,id:req.user._id,작성자: req.user.id ,제목 : req.body.title, 내용 :req.body.data,시간:new Date()}
+var 저장할거 = {_id:총게시물갯수+1,
+                id:req.user._id,
+                작성자: req.user.id ,
+                제목 : req.body.title, 
+                내용 :req.body.data,
+                시간:new Date()}
 
 db.collection('post').insertOne(저장할거,(에러,결과)=>{
     console.log('저장완료');
@@ -178,51 +163,37 @@ db.collection('counter').updateOne({name:'총게시물갯수'},{$inc: {totalPost
 });
 });
 
-});
+}); // qna 글쓰기 
 
 app.get('/qna',(req,res)=>{
 
-db.collection('post').find().sort({"시간":-1}).toArray(function(에러,결과){
-res.render('qna.ejs',{posts:결과});
+db.collection('post').find().sort({"시간":-1}).toArray(function(error,result){
+res.render('qna.ejs',{posts:result});
 
 })
-})
-
-
+}) //qna 페이지
 
 app.get('/detail/:id',로그인했니,(req,res)=>{
-
-
-db.collection('post').findOne({_id:parseInt(req.params.id)},(error,result)=>{
-
-db.collection('comment').find({글번호:req.params.id}).sort({"시간":-1}).toArray((error,result2)=>{
-    
-    
-    res.render('detail.ejs',{data:result,commentdata:result2,dataid:req.user});
-})
-})
-
-})
-
+    db.collection('post').findOne({_id:parseInt(req.params.id)},(error,result)=>{
+        db.collection('comment').find({글번호:req.params.id}).sort({"시간":-1}).toArray((error,result2)=>{ 
+            res.render('detail.ejs',{data:result,commentdata:result2,dataid:req.user});
+        })
+    })
+}) // qna 디테일 페이지
 
 app.post('/comment',로그인했니,(req,res)=>{
-    var 저장= {댓글 : req.body.comment,글번호 : req.body.parentnumber ,댓글작성자: req.user.id, 시간: new Date()}
+    var 저장= { 댓글 : req.body.comment,
+                글번호 : req.body.parentnumber ,
+                댓글작성자: req.user.id, 
+                시간: new Date()}
     db.collection('comment').insertOne(저장,(error,result)=>{
-        
-        console.log('저장완료');
-
-        
-        // res.render('detail.ejs',{commentdata:result,data:result,dataid:req.user});
         res.send('<script>window.location="/detail/'+ req.body.parentnumber+'"</script>')   
-    
-
-        
-
-    
     })
-})  
+})  // qna 댓글
+
 app.get('/search',(req,res)=>{
-var 검색조건 = [
+var 검색조건 = 
+[
 {
     $search: {
         index: 'pocaSearch',
@@ -231,10 +202,7 @@ var 검색조건 = [
         path: '제목'  // 제목날짜 둘다 찾고 싶으면 ['제목', '날짜']   
         }
     }
-    },
-    //{$progect : {}} // 원하는 것만 보여주고 싶을 때 
-    
-    
+    },      
 ]
 console.log(req.query.value);
 db.collection('post').aggregate(검색조건).toArray((error,result)=>{ 
@@ -245,14 +213,11 @@ res.render('search.ejs', {posts : result});
 
 })
 
-})
-  
-
+})// 서치페이지
 
 app.get('/scam',(req,res)=>{
     res.render('scam.ejs');
-})
-
+}) //사기조회
 
 app.get('/best',(req,res)=>{
     db.collection('content').find().sort({"조회수":-1}).toArray((error,result)=>{
@@ -260,18 +225,9 @@ app.get('/best',(req,res)=>{
         if(error) {
             console.log(error);
         }
-    
-        console.log(result);
-
         res.render('best.ejs',{Module:result});
 })
-})
-
-
-
-app.get('bestLike',(req,res)=>{
-res.render('bestLike.ejs');
-})
+}) //베스트 페이지
 
 
 app.get('/new',(req,res)=>{
@@ -281,11 +237,10 @@ app.get('/new',(req,res)=>{
             console.log(error);
         }
     
-        console.log(result);
+      
 res.render('new.ejs',{Module:result});
 })
-})
-
+}) // 새페이지
 
 app.get('/poca',(req,res)=>{
     db.collection('content').find().toArray((error,result)=>{
@@ -302,7 +257,7 @@ app.get('/poca',(req,res)=>{
 
 
         })
-})
+}) // poca페이지
 
 app.get('/poca/:middle',(req,res)=>{
     var value = {middle : req.params.middle};
@@ -314,15 +269,9 @@ app.get('/poca/:middle',(req,res)=>{
             if(error) {
                 console.log(error);
             }
-        
-    
-          
-    
-                res.render('poca.ejs', {poca:result2,category:result}); //변수 여러개 줘도 상관 없음, result==category
-    
-    
+          res.render('poca.ejs', {poca:result2,category:result}); //변수 여러개 줘도 상관 없음, result==category
+
             })
-    
     }) //제목 없이 글쓰기 금지
 })
 
@@ -368,7 +317,7 @@ app.post('/addwirte',upload.single('picture'),(req,res)=>{
         var totalNumber=result.totalContent;
         
         var save = {_id:totalNumber+1 ,id:req.user._id,작성자: req.user.id,제목 : req.body.pocatitle, 대분류 : req.body.group, 
-            middle : req.body.issue, 소분류 : req.body.detail, 값 : req.body.price, 설명 : req.body.explanation, 
+            middle : req.body.issue, 소분류 : req.body.detail, 값 : req.body.price, 설명 : req.body.explanation, 좋아요:0,
             조회수 : 0,시간: new Date(),이미지:req.file?.originalname?req.file.originalname:null}
         
         db.collection('content').insertOne(save,(에러,결과)=>{
@@ -384,10 +333,6 @@ app.post('/addwirte',upload.single('picture'),(req,res)=>{
         });
         
         });
-        
-
-
-
 
 app.get('/pocadetail/:id',(req,res)=>{
     db.collection('content').findOne({_id:parseInt(req.params.id)},(error,result)=>{
@@ -397,48 +342,72 @@ app.get('/pocadetail/:id',(req,res)=>{
         res.render('pocadetail.ejs',{contents:result});
     })
 })
-//찜 구현
+
 app.post('/like',(req,res)=>{
-    var Data=req.body; // 글 작성자 아이디
-    var data = {id:req.user.id}; // 로그인 한 아이디
-    var json = JSON.stringify(Data);
-    var json1 = JSON.stringify(data);
+    var Data=req.body.id; // 글 작성자 아이디
+    var data = req.user.id; // 로그인 한 아이디
+    
+   
+    
+    
         if(!req.user){  //로그인 안됨
            return res.render('login.ejs');
         } // 리턴을 사용하면 끝나는 부분
         
         
-        if(json===json1){
+        if(Data===data){
             console.log('자신의 게시물에는 좋아요를 누를 수 없음');
         }
         else{
-            db.collection('like').findOne({찜아이디:req.user.id,게시물번호:req.body.num},(error,result)=>{
+            db.collection('like').findOne({찜아이디:req.user.id,게시물번호:parseInt(req.body._id)},(error,result)=>{
+                db.collection('content').find({_id:parseInt(req.body._id)}).toArray((error,result2)=>{
+                    console.log(result2)
                 if(result){ // result가 참일때 이렇게 쓰면 조음
                 db.collection('like').deleteOne({찜아이디:req.user.id})
+                db.collection('content').updateOne({_id:parseInt(req.body._id)},{$pull:{likeid:data},$inc:{좋아요:-1}},(result)=>{
+                })
                 }
                 else{
-                    var 저장 = {게시물번호:req.body.num,찜아이디: req.user.id,시간:new Date()}
-                    db.collection('like').insertOne(저장)  //늦게 동작해도 상관 없으면 괜찬, 아니면 방법 생각
-                }
-
+                    var 저장 = {게시물번호:parseInt(req.body._id),찜아이디: req.user.id,시간:new Date()}
+                    db.collection('like').insertOne(저장,(error,result)=>{
+                    })  //늦게 동작해도 상관 없으면 괜찬, 아니면 방법 생각
+                        
+                    db.collection('content').updateOne({_id:parseInt(req.body._id)},{$addToSet:{likeid:data},$inc:{좋아요:1}},(result)=>{
+                    }
+                        
+                )}
+                    
+            })  
+        
             })
         }      
-})
+}) //찜 구현
+   
 
-
-
+app.get('/bestLike',(req,res)=>{
+    db.collection('content').find().sort({"좋아요":-1}).toArray((error,result2)=>{
+        res.render('bestLike.ejs', {Module:result2});
+        console.log(result2);
+        })
+        }) // 찜페이지
+   
 
 app.post('/chatroom',로그인했니,(req,res)=>{
+    console.log(req.body);
+    console.log(req.user);
+    
     var save = {
-        title : req.body.당한사람,
-        member : [ObjectId(req.body.당한사람id), req.user._id],
+        title : req.body.chatroomname,
+        member : [ObjectId(req.body.chatroomid), req.user._id],
         date : new Date()
       }
+
+      console.log(save);
     
       db.collection('chatroom').insertOne(save).then((result)=>{
-          res.send('저장완료')
+        console.log(result);
       });
-})
+}) //채팅방
 
 app.get('/chat',로그인했니,(req,res)=>{
     db.collection('chatroom').find({member : req.user._id}).toArray().then((result)=>{ // member -> 현재 로그인한 유저의 _id
@@ -468,7 +437,6 @@ app.get('/message/:parentid', 로그인했니, function(request, response){
   
     db.collection('message').find({ parent: request.params.parentid }).toArray()
     .then((result)=>{
-      console.log(result);
       response.write('event: test\n');
       response.write(`data: ${JSON.stringify(result)}\n\n`);
     });
@@ -479,7 +447,6 @@ app.get('/message/:parentid', 로그인했니, function(request, response){
     const changeStream = db.collection('message').watch(findDocu);
     changeStream.on('change', result => {
       var addDocu = [result.fullDocument];
-      console.log(addDocu);
       response.write('event: test\n');
       response.write(`data: ${JSON.stringify(addDocu)}\n\n`);
     });
